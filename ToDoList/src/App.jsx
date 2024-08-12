a59c9a3f1c090174a0f1d1ccf53b4dd93dcc2443
Clara Helena <clarahelenasena@gmail.com>
2024-08-12 10:53:52 -0400

    first commit

---
+++ ToDoList/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="border-b border-slate-400">
+      <h1 className="text-3xl font-semibold">
+        Minha Lista de Tarefas do dia
+      </h1>
+    </div>
+  );
+}
+
+export default App;
